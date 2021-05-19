942273bf02f89885db44e68bbf5da23e6d2be457
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-05-19 23:36:32 +0900

    コメント一覧表示機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -29,7 +29,7 @@ class TweetsController < ApplicationController
 
   def show
     @comment = Comment.new
-    @comments = @tweet.comments.include(:user)
+    @comments = @tweet.comments.includes(:user)
   end
 
   private
